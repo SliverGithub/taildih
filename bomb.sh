0837ae5beec16305cb428075cfda91fd76bb6a61
SliverGithub <75800432+SliverGithub@users.noreply.github.com>
2025-11-02 13:17:39 +0100

    Update Tailscale installation commands in bomb.sh

--- bomb.sh
+++ bomb.sh
@@ -1,3 +1,6 @@
-curl -fsSL https://tailscale.com/install.sh | sh
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 sudo systemctl enable --now tailscaled
 sudo tailscale up --ssh --authkey=tskey-auth-kALBhvex5A21CNTRL-bfpn7weiajSVuAPQmL31jSfhKCfiCj41
